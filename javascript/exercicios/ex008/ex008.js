cb9c50b9b26b80b6b6f203557e55b3ef6128fb20
Murillo Silveira Horvath <116307052+MurilloHorvath@users.noreply.github.com>
2022-11-15 15:45:25 -0300

    aula 11

--- javascript/exercicios/ex008/ex008.js
+++ javascript/exercicios/ex008/ex008.js
@@ -1,1 +1,6 @@
-console.log(`ola mundo`)
+var vel = 70
+console.log(`a velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){
+    console.log('voce ultrapassou a velocidade permitida... Multado')
+}
+console.log('dirija sempre usando cinto de segurança')
