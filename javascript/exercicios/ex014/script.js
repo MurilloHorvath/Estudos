d949a81a685816fb9dea7011248ac33a711be70c
Murillo Silveira Horvath <116307052+MurilloHorvath@users.noreply.github.com>
2022-11-16 15:56:16 -0300

    ex014

--- javascript/exercicios/ex014/script.js
+++ javascript/exercicios/ex014/script.js
@@ -1,1 +1,22 @@
-alert('Ola mundo')
+function carregar(){
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+
+
+msg.innerHTML = `Agora são ${hora} Horas`
+if (hora >= 0 && hora < 12){
+    //img bom dia
+    img.src = 'imagens/manha.png'
+    document.body.style.background = '#F27405'
+}else if(hora >= 12 && hora <= 18){
+    //img tarde
+    img.src = 'imagens/tarde.png'
+    document.body.style.background = '#225473'
+}else{
+    //img noite
+    img.src = 'imagens/noite.png'
+    document.body.style.background = '#403231'
+}
+}
