131d4bfddbc37050cbe12b09cb53a06e441e4113
Murillo Silveira Horvath <116307052+MurilloHorvath@users.noreply.github.com>
2023-02-15 15:55:15 -0300

    15/02

--- javascript/aulas react/meu-projeto/src/App.js
+++ javascript/aulas react/meu-projeto/src/App.js
@@ -1,27 +1,72 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld'
-import SayMyName from './components/SayMyName'
-import Pessoa from './components/Pessoa'
-import List from './components/List'
-import Evento from './components/Evento'
-import Form from './components/Form'
+// import HelloWorld from './components/HelloWorld'
+// import SayMyName from './components/SayMyName'
+
+// import Pessoa from './components/Pessoa'
+// import List from './components/List'
+
+// import Evento from './components/Evento'
+// import Form from './components/Form'
+
+// import Condicional from './components/Condicional';
+
+// import OutraLista from './components/OutraLista';
+
+// import SeuNome from './components/SeuNome';
+// import Saudacao from './components/Saudacao';
+
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Contato from './pages/Contato';
+import Empresa from './pages/Empresa';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+
+// import { useState } from 'react';
 
 function App() {
 
-  const nome = "Jasmin"
+  // const [seuNome, setSeuNome] = useState()
+
+  // const meusItens = ["React", "Vue", "Angular"]
+
+  // const nome = "Jasmin"
 
   return (
-    <div className="App">
-      <HelloWorld />
-      <SayMyName nome="Murillo"/>
-      <SayMyName nome={nome}/>
-      <Pessoa nome="Murillo" idade="19" profissao="Técnico em eletrônica" foto="https://via.placeholder.com/150" />
-      <List/>
-      <h1>Testando evento</h1>
-      <Evento numero="1" />
-      <Evento numero="2" />
-      <Form/>
-    </div>
+    // <div className="App">
+    //   <HelloWorld />
+    //   <SayMyName nome="Murillo"/>
+    //   <SayMyName nome={nome}/>
+
+    //   <Pessoa nome="Murillo" idade="19" profissao="Técnico em eletrônica" foto="https://via.placeholder.com/150" />
+    //   <List/>
+
+    //   <h1>Testando evento</h1>
+    //   <Evento numero="1" />
+    //   <Form/>
+
+    //   <h1>Renderização Condicional</h1>
+    //   <Condicional/>
+
+    //   <h1>Renderização de Listas</h1>
+    //   <OutraLista itens={meusItens} />
+    //   <OutraLista itens={[]} />
+
+    //   <h1>State Lift</h1>
+    //   <SeuNome setNome={setSeuNome}/>
+    //   <Saudacao nome={seuNome} />
+    // </div>
+
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} ></Route>
+          <Route path="/empresa" element={<Empresa />} ></Route>
+          <Route path="/contato" element={<Contato />} ></Route>
+        </Routes>
+        <Footer />
+      </Router>
+
   )
 }
 
--- javascript/aulas react/meu-projeto/src/components/Evento.js
+++ javascript/aulas react/meu-projeto/src/components/Evento.js
@@ -1,12 +1,20 @@
+import Button from './evento/Button'
+
 function Evento({numero}) {
 
     function meuEvento() {
         console.log(`Opa, fui ativado! ${numero}`)
     }
 
+    function segundoEvento() {
+        console.log("Ativando o segundo evento!")
+    }
+
     return (
         <div>
             <p>Clique para disparar um evento: </p>
+            <Button event={meuEvento} text="Primeiro Evento" />
+            <Button event={segundoEvento} text="Segundo Evento" />
             <button onClick={meuEvento}>Ativar</button>
         </div>
     )
