a95971285918850db63a1c2971c37ae7d9b3f23d
Murillo Silveira Horvath <116307052+MurilloHorvath@users.noreply.github.com>
2023-02-16 15:17:56 -0300

    16/02

--- javascript/aulas react/meu-projeto/src/components/layout/Footer.js
+++ javascript/aulas react/meu-projeto/src/components/layout/Footer.js
@@ -1,5 +1,15 @@
+import { BsFacebook,BsInstagram,BsLinkedin } from "react-icons/bs";
+
+import styles from './Footer.module.css'
+
 function Footer() {
-    return <footer>Rodapé</footer>
+    return (
+        <ul className={styles.social_list}>
+            <li><BsFacebook/></li>
+            <li><BsInstagram/></li>
+            <li><BsLinkedin/></li>
+        </ul>
+    )
 }
 
 export default Footer
