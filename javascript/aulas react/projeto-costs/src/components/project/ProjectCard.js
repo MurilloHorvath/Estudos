111a0cf0afbd001fe37957a866d25fca07804bc0
Murillo Silveira Horvath <116307052+MurilloHorvath@users.noreply.github.com>
2023-02-22 13:36:31 -0300

    22/02

--- javascript/aulas react/projeto-costs/src/components/project/ProjectCard.js
+++ javascript/aulas react/projeto-costs/src/components/project/ProjectCard.js
@@ -19,7 +19,7 @@ function ProjectCard({id, name, budget, category, handleRemove}) {
             <p className={styles.category_text}>
                 <span className={`${styles[category.toLowerCase()]}`}></span> {category}</p>
             <div className={styles.project_card_actions}>
-                <Link tp="/">
+                <Link to={`/project/${id}`}>
                     <BsPencil /> Editar
                 </Link>
                 <button onClick={remove}>
