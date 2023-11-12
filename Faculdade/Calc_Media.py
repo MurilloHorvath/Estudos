aa5b0cc1f92307ab8bee7ebc7ce7f1e7e1a0eb7f
Murillo Silveira Horvath <116307052+MurilloHorvath@users.noreply.github.com>
2023-11-12 10:45:03 -0300

    atividade uniasselvi
    Começo da primeira atividade relacionada a desenvolvimento de site...

---
+++ Faculdade/Calc_Media.py
@@ -0,0 +1,8 @@
+nota1 = int(input('Nota 1 = '))
+nota2 = int(input('Nota 2 = '))
+nota3 = int(input('Nota 3 = '))
+nota4 = int(input('Nota 4 = '))
+
+s =  ( ( nota1 * 1.5 ) + ( nota2 * 1.5 ) + ( nota3 * 4 ) + ( nota4 * 3 ) ) / 10
+
+print('a media do aluno foi ', s)
