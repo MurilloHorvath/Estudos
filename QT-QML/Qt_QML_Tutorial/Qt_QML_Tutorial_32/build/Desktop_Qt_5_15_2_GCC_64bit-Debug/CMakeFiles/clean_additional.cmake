be079766f49289db52b3a869b3691f171ba11688
Murillo Silveira Horvath <116307052+MurilloHorvath@users.noreply.github.com>
2025-09-29 16:30:25 -0300

    [CLEAN] BUG Build QML

--- QT-QML/Qt_QML_Tutorial/Qt_QML_Tutorial_32/build/Desktop_Qt_5_15_2_GCC_64bit-Debug/CMakeFiles/clean_additional.cmake
+++
@@ -1,7 +0,0 @@
-# Additional clean files
-
-file(REMOVE_RECURSE
-  "CMakeFiles/Qt_QML_Tutorial_32_autogen.dir/AutogenUsed.txt"
-  "CMakeFiles/Qt_QML_Tutorial_32_autogen.dir/ParseCache.txt"
-  "Qt_QML_Tutorial_32_autogen"
-)
